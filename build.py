7037eee6cf414adcb991774d9a40e041fd447198
Jack Minchin <jack@thisisstratum.co.uk>
2021-08-03 13:05:29 +0100

    fixed build.py

--- build.py
+++ build.py
@@ -2,8 +2,7 @@ import os
 os.system('python -m ipykernel install --user')
 os.system('jupyter-book build . --all --builder dirhtml')
 os.system('python ./utils/analytics.py')
-
-#os.system('python ./utils/setIndex.py')
+os.system('python ./utils/setIndex.py')
 
 
 #os.system('jupyter-book build . --builder pdflatex')
